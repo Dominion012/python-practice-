2c154c9cfc7eab4f1312227a083ba0f6cf0d9c94
Dominion Arogunjo <dominionarogunjo@Dominions-MacBook-Pro.local>
2026-05-07 20:24:15 -0400

    Day 1 - Python variables and data types

---
+++ day1.py
@@ -0,0 +1,10 @@
+# Day 1 = python -practice 
+# variables and data types 
+
+name = "Domi"
+age = 23 
+is_learning = True
+
+print(f" my name is {name}")
+print(f"my age is {age}")
+print("Am i learning code?" , is_learning)
