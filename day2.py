6c535aa9be47af5544586c8948fe3efc2b640ed0
Dominion Arogunjo <dominionarogunjo@Dominions-MacBook-Pro.local>
2026-05-11 10:58:42 -0400

    Day 2 - Lists and loops

---
+++ day2.py
@@ -0,0 +1,13 @@
+# practicing lists using a list of my_hobbies as an example 
+
+my_hobbies = ["Video games", "Movies", "Reading bible"]
+
+# print(my_hobbies)
+#print(my_hobbies[0])
+#print(len(my_hobbies))
+
+#for hobbies in my_hobbies:
+    #print (hobbies)
+
+for hobbies in my_hobbies:
+    print(f"I enjoy {hobbies}")
